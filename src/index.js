d61c2190997a47b7624d79c6204f8a32b3649f13
rudolfdeer <tomalichko@gmail.com>
2021-02-02 12:29:09 +0300

    feat: add function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length <= 1) return false;
+
+    let matchingOpeningBr, ch;
+    let stack = [];
+
+    let openingBr = [];
+    let closingBr = [];
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        openingBr.push(bracketsConfig[i][0]);
+        closingBr.push(bracketsConfig[i][1]);
+    }
+
+    let intersection = openingBr.filter((x) => closingBr.includes(x));
+
+    for (let i = 0; i < str.length; i++) {
+        ch = str[i];
+
+        if (closingBr.indexOf(ch) > -1 && !intersection.includes(ch)) {
+            matchingOpeningBr = openingBr[closingBr.indexOf(ch)];
+
+            if (stack.length == 0 || stack.pop() != matchingOpeningBr) {
+                return false;
+            }
+        } else if (intersection.includes(ch)) {
+            if (!intersection.includes(stack[stack.length - 1])) {
+                stack.push(ch);
+            } else {
+                stack.pop();
+            }
+        } else {
+            stack.push(ch);
+        }
+    }
+
+    return stack.length == 0;
+};
