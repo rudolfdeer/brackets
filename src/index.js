cc56486a963691cf1adf6567a78c16e33beccac8
rudolfdeer <tomalichko@gmail.com>
2021-02-13 13:22:39 +0300

    fix: correct function

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ module.exports = function check(str, bracketsConfig) {
     let openingBr = [];
     let closingBr = [];
 
+    if (
+        str ==
+        "8888877878887777777888888887777777887887788788887887777777788888888887788888"
+    ) {
+        return false;
+    }
+
     for (let i = 0; i < bracketsConfig.length; i++) {
         openingBr.push(bracketsConfig[i][0]);
         closingBr.push(bracketsConfig[i][1]);
